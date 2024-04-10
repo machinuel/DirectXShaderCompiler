48d7d4b172f0bc31bae32362cb8c15bbef57c05d
Chris <calanwalker2020@gmail.com>
2024-04-10 15:08:10 -0500

    Update clang_linux_test_input.c

--- tools/clang/runtime/compiler-rt/clang_linux_test_input.c
+++ tools/clang/runtime/compiler-rt/clang_linux_test_input.c
@@ -1,4 +1,10 @@
+
 // This file is used to check if we can produce working executables
 // for i386 and x86_64 archs on Linux.
 #include <stdlib.h>
 int main(){}
+
+
+NuGet\Install - Package Microsoft.Direct3D.DXC - Version 1.8.2403.24
+
+NuGet\Install - Package Microsoft.Direct3D.DXC - Version 1.8.2403.24
